32c8a52a25a589dbecbce51a7e0ea8ed507dce96
Sumit Kumar <sharansumitkumar@gmail.com>
2026-05-21 16:45:08 +0530

    code updated

--- lib/core/constants/editor_web_constants.dart
+++ lib/core/constants/editor_web_constants.dart
@@ -1,6 +1,6 @@
-/// The URL of the web worker script.
+/// The URL of the web worker script for web builds.
 ///
 /// This URL points to the JavaScript file that will be loaded by the
 /// [web.Worker] instance to run the web worker's code.
 const kImageEditorWebWorkerPath =
-    'assets/packages/pro_image_editor/lib/web/web_worker.dart.js';
+    'assets/packages/pro_image_editor/web/web_worker.dart.js';
